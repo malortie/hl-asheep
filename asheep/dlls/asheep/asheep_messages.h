30a9985bf5bcdd7513c7829c17bf76d17bcbe7a2
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-05-09 10:33:56 -0400

    Removed asheep directory.

--- asheep/dlls/asheep/asheep_messages.h
+++
@@ -1,9 +0,0 @@
-#ifndef ASHEEP_MESSAGES_H__
-#define ASHEEP_MESSAGES_H__
-
-void Message_CreateAndSetupSingleton();
-void Message_ShowMessage(const char* message);
-void Message_RemoveTrainingMapMessages();
-BOOL Message_MessagesAllowedInCurrentMap();
-
-#endif // ASHEEP_MESSAGES_H__
--- asheep/dlls/asheep/scientist.h
+++
@@ -1,7 +0,0 @@
-#ifndef MONSTER_SCIENTIST_H__
-#define MONSTER_SCIENTIST_H__
-
-#define		NUM_SCIENTIST_HEADS		4 // four heads available for scientist model
-enum { HEAD_GLASSES = 0, HEAD_EINSTEIN = 1, HEAD_LUTHER = 2, HEAD_SLICK = 3 };
-
-#endif // MONSTER_SCIENTIST_H__
