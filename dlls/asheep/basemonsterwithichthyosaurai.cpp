8620855b3ea58548f79925882b95487ec99fee32
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-05-15 16:45:49 -0400

    Use std::abs.

--- dlls/asheep/basemonsterwithichthyosaurai.cpp
+++ dlls/asheep/basemonsterwithichthyosaurai.cpp
@@ -814,15 +814,15 @@ void CBaseMonsterWithIchthyosaurAI::Swim()
 	float turn = 360;
 	// ALERT( at_console, "Y %.0f %.0f\n", Angles.y, pev->angles.y );
 
-	if (fabs(Angles.y - pev->angles.y) < fabs(turn))
+	if (std::abs(Angles.y - pev->angles.y) < std::abs(turn))
 	{
 		turn = Angles.y - pev->angles.y;
 	}
-	if (fabs(Angles.y - pev->angles.y + 360) < fabs(turn))
+	if (std::abs(Angles.y - pev->angles.y + 360) < std::abs(turn))
 	{
 		turn = Angles.y - pev->angles.y + 360;
 	}
-	if (fabs(Angles.y - pev->angles.y - 360) < fabs(turn))
+	if (std::abs(Angles.y - pev->angles.y - 360) < std::abs(turn))
 	{
 		turn = Angles.y - pev->angles.y - 360;
 	}
@@ -830,7 +830,7 @@ void CBaseMonsterWithIchthyosaurAI::Swim()
 	float speed = m_flightSpeed * 0.1;
 
 	// ALERT( at_console, "speed %.0f %f\n", turn, speed );
-	if (fabs(turn) > speed)
+	if (std::abs(turn) > speed)
 	{
 		if (turn < 0.0)
 		{
@@ -856,20 +856,20 @@ void CBaseMonsterWithIchthyosaurAI::Swim()
 	// Roll Smoothing
 	//
 	turn = 360;
-	if (fabs(Angles.z - pev->angles.z) < fabs(turn))
+	if (std::abs(Angles.z - pev->angles.z) < std::abs(turn))
 	{
 		turn = Angles.z - pev->angles.z;
 	}
-	if (fabs(Angles.z - pev->angles.z + 360) < fabs(turn))
+	if (std::abs(Angles.z - pev->angles.z + 360) < std::abs(turn))
 	{
 		turn = Angles.z - pev->angles.z + 360;
 	}
-	if (fabs(Angles.z - pev->angles.z - 360) < fabs(turn))
+	if (std::abs(Angles.z - pev->angles.z - 360) < std::abs(turn))
 	{
 		turn = Angles.z - pev->angles.z - 360;
 	}
 	speed = m_flightSpeed / 2 * 0.1;
-	if (fabs(turn) < speed)
+	if (std::abs(turn) < speed)
 	{
 		pev->angles.z += turn;
 	}
