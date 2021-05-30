3f0a412354b760ba940d1782ce483bfb3a870450
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-05-30 13:37:55 -0400

    Added missing entity monster_myself_dead.
    It is present in the DLL entities, but it is unused.

---
+++ dlls/asheep/dead_myself.cpp
@@ -0,0 +1,47 @@
+/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+//=========================================================
+// monster template
+//=========================================================
+// UNDONE: Holster weapon?
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "gamerules.h"
+#include "monsters.h"
+#include "dead_actor.h"
+
+//=========================================================
+// Dead Myself PROP
+//=========================================================
+class CDeadMyself : public CDeadActorWithBarneySequences
+{
+public:
+	virtual void PrecacheModels();
+	virtual void SetModel();
+};
+
+void CDeadMyself::PrecacheModels()
+{
+	PRECACHE_MODEL("models/myself.mdl");
+}
+
+void CDeadMyself::SetModel()
+{
+	SET_MODEL(ENT(pev), "models/myself.mdl");
+}
+
+LINK_ENTITY_TO_CLASS(monster_myself_dead, CDeadMyself);
