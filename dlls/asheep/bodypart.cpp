66d905c9f48232b32d5c058e1b861572eea97aef
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-06-06 14:02:36 -0400

    Implemented missing monster_bodypart behavior.

--- dlls/asheep/bodypart.cpp
+++ dlls/asheep/bodypart.cpp
@@ -18,21 +18,88 @@
 #include "cbase.h"
 #include "studio.h"
 
+#define GIB_HUMAN_MODEL		"models/hgibs.mdl"
+#define GIB_HGRUNT_MODEL	"models/gib_hgrunt.mdl"
+
+enum BodypartType
+{
+	BP_ANY = 0,
+	BP_HGIBS_ONLY = 1,
+	BP_HGRUNTGIBS_ONLY = 2,
+	BP_NUM_TYPES, // Must be last.
+};
+
+int GetModelCountForBodyGroup(edict_t* edict, int iBodyGroup);
+
 class CBodyPart : public CBaseAnimating
 {
 public:
 	void Spawn(void);
 	virtual int	ObjectCaps(void) { return (CBaseAnimating::ObjectCaps() & ~FCAP_ACROSS_TRANSITION); }
-
-	virtual void SetSkinAndBodygroups();
 };
 
 LINK_ENTITY_TO_CLASS(monster_bodypart, CBodyPart);
 
 void CBodyPart::Spawn()
 {
-	PRECACHE_MODEL("models/hgibs.mdl");
-	SET_MODEL(ENT(pev), "models/hgibs.mdl");
+	// Ensure body is non negative.
+	if (pev->body < 0)
+		pev->body = 0;
+
+	const char* model_name = NULL;
+
+	if (pev->body == BodypartType::BP_ANY)
+	{
+		if (RANDOM_LONG(0, 15) > 0)
+			model_name = GIB_HUMAN_MODEL;
+		else
+			model_name = GIB_HGRUNT_MODEL;
+	}
+	else if (pev->body == BodypartType::BP_HGIBS_ONLY)
+	{
+		model_name = GIB_HUMAN_MODEL;
+	}
+	else if (pev->body == BodypartType::BP_HGRUNTGIBS_ONLY)
+	{
+		model_name = GIB_HGRUNT_MODEL;
+	}
+	else
+	{
+		// Undefined setup. Use a fallback model.
+		model_name = GIB_HUMAN_MODEL;
+	}
+
+	PRECACHE_MODEL((char*)model_name);
+	SET_MODEL(ENT(pev), model_name);
+
+	if (pev->body < BodypartType::BP_NUM_TYPES)
+	{
+		// Reset pev->body.
+		pev->body = 0;
+
+		// Choose a random bodygroup if multiple are available.
+		int numberOfAvailableSubModels = GetModelCountForBodyGroup(ENT(pev), 0);
+		if (numberOfAvailableSubModels > 0)
+			SetBodygroup(0, RANDOM_LONG(0, numberOfAvailableSubModels - 1));
+	}
+	else
+	{
+		// Convert from body type to bodygroup index.
+		pev->body -= BodypartType::BP_NUM_TYPES;
+
+		// Undefined setup. Set the bodygroup as it is, but ensure it is less than the max available.
+		int numberOfAvailableSubModels = GetModelCountForBodyGroup(ENT(pev), 0);
+		if (numberOfAvailableSubModels > 0)
+		{
+			pev->body = std::min(pev->body, numberOfAvailableSubModels - 1);
+			SetBodygroup(0, pev->body);
+		}
+		else
+		{
+			// Reset pev->body.
+			pev->body = 0;
+		}
+	}
 
 	pev->movetype = MOVETYPE_NONE;
 	pev->solid = SOLID_NOT;
@@ -45,10 +112,8 @@ void CBodyPart::Spawn()
 
 	ResetSequenceInfo();
 	pev->frame = 0;
-	SetSkinAndBodygroups();
 
-	if (pev->body < 0)
-		pev->body = 0;
+	pev->skin = 0;
 
 	SetThink(&CBodyPart::SUB_DoNothing);
 
@@ -61,13 +126,4 @@ int GetModelCountForBodyGroup(edict_t* edict, int iBodyGroup)
 	mstudiobodyparts_t *pbodypart = (mstudiobodyparts_t *)((byte *)pstudiohdr + pstudiohdr->bodypartindex) + iBodyGroup;
 
 	return pbodypart->nummodels;
-}
-
-void CBodyPart::SetSkinAndBodygroups()
-{
-	pev->skin = 0;
-
-	int numberOfAvailableSubModels = GetModelCountForBodyGroup(ENT(pev), 0);
-	if (numberOfAvailableSubModels > 0)
-		SetBodygroup(0, RANDOM_LONG(0, numberOfAvailableSubModels - 1));
 }
